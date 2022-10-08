9707efc116305294d038b49964c1c3857c0c43c6
Diogo Pereira <diogopereira5.ds@gmail.com>
2022-10-08 02:08:03 -0300

    feat: neural

--- scripts/neural_network.gd
+++ scripts/neural_network.gd
@@ -1,27 +1,44 @@
 class_name Neural_Network
 
 var weights_inputs: Array = []
-var weights_outputs: Array = []
+var weights_output: Array = []
+
+var bias: float = 1
+
+func sigmoid(x):
+	return 1 / (1 + exp(-x))
 
 func start(size_inputs,size_hidden,size_output):
 	randomArray(size_inputs,size_hidden,weights_inputs)
-	randomArray(size_hidden,size_output,weights_outputs)
+	randomArray(size_hidden,size_output,weights_output)
 	
-	#print(weights_inputs)
-	#print(weights_outputs)
+func think(id_tile_map):
+	var inputs: Array = [id_tile_map]
+	var hidden_layer = multiply(inputs,weights_inputs)
+	var output = multiply(hidden_layer, weights_output)
+	print(output)
 	
-func think(pos: Vector2):
-	pass
-
+	
+func multiply(matrix1, matrix2):
+	var arr: Array = []
+	for i in matrix2:
+		var temp: float
+		for j in i.size():
+			var value = i[j] * matrix1[j]
+			temp += value
+		arr.append(sigmoid(temp + bias))
+	return arr
+	
 #criar matrix randomica
 func randomArray(i_size, j_size, arr):
 	
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	
-	for i in i_size:
+	for i in j_size:
 		var temp: Array = []
-		for j in j_size:
-			var random_number = rng.randf_range(0,1)
+		for j in i_size:
+			var random_number = rng.randf_range(-1,1)
 			temp.append(random_number)
 		arr.append(temp)
+
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var NeuralNetwork = preload("res://scripts/neural_network.gd")
 
-onready var player := get_tree().get_root().get_node("Main").get_node("Map")
+onready var Map := get_tree().get_root().get_node("Main").get_node("Map")
 
 export (int) var speed = 100
 var velocity = Vector2()
@@ -10,11 +10,18 @@ var neural
 
 func _init():
 	neural = NeuralNetwork.new()
-	neural.start(4,4,4)
+	neural.start(1,10,4)
 
 func _physics_process(delta):
 	move(delta)
-	#neural.think(get_transform()[2].normalized())
+	
+	# pegar atributos para a rede neural
+	var pos = get_transform()[2]
+	var tilemap = Map.get_node("floor")
+	var tile:Vector2 = tilemap.world_to_map(pos)
+	var cell_id:int = tilemap.get_cellv(tile)
+	neural.think(cell_id)
+	
 	
 func move(delta):
 	get_input()
@@ -35,4 +42,5 @@ func get_input():
 	if Input.is_action_pressed("right"):
 		velocity += Vector2(1,0.5)
 		$Sprite.set_frame(5)
+		
 	velocity = velocity.normalized() * speed
